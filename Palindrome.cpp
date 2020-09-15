3fb9e55093e99fba69eec18a83f1e591d037cdae
AndyF-42 <65932590+AndyF-42@users.noreply.github.com>
2020-09-15 09:48:39 -0700

    Final commit

--- Palindrome.cpp
+++ Palindrome.cpp
@@ -5,32 +5,36 @@ using namespace std;
 
 int main() {
 
+  char input[80];
   char str1[80];
   char str2[80];
-
-  cin.get(str1, 80);
+  cin.get(input, 80);
   cin.get();
+
+  //take only the alphanumeric chars from input
+  //and put them lowercase into str1 
+  int count = 0;
+  for (int i = 0; i < strlen(input); i++) {
+    if (isalnum(input[i])) {
+      str1[count] = tolower(input[i]);
+      count++;
+    }
+  }
+  //add the null character, copy into str2
+  str1[count] = '\0';
   strcpy(str2, str1);
 
-  cout << str1 << endl;
-  cout << str2 << endl;
-
-  cout << str1[0] << endl;
-  cout << str2[strlen(str1) - 1] << endl;
-  if (str1[0] != str2[strlen(str1) - 1]) {
-    cout << "same" << endl;
-  }
-  
+  //check chars going forwards from str1 and going backwards from str2
   bool isPalindrome = true;
   for (int i = 0; i < strlen(str1); i++) {
-    
     if (str1[i] != str2[strlen(str1) - i - 1]) {
       isPalindrome = false;
     }
   }
 
-  if (isPalindrome == true) {
-    cout << "It's a palindrome!" << endl;
+  //proper output
+  if (isPalindrome) {
+    cout << "Palindrome." << endl;
   } else {
     cout << "Not a palindrome." << endl;
   }
