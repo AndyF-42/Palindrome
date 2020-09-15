1df51a4468e680316d231ab84e0f118b54369496
AndyF-42 <65932590+AndyF-42@users.noreply.github.com>
2020-09-15 09:03:04 -0700

    First commit

---
+++ Palindrome.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <cstring>
+
+using namespace std;
+
+int main() {
+
+  char str1[80];
+  char str2[80];
+
+  cin.get(str1, 80);
+  cin.get();
+
+  cout << strlen(str1) << endl; 
+
+
+  return 0;
+}
