613d53bad4393946f71134103ed8c4834e12c742
AndyF-42 <65932590+AndyF-42@users.noreply.github.com>
2020-09-15 09:16:41 -0700

    Palindrome functionality

--- Palindrome.cpp
+++ Palindrome.cpp
@@ -10,9 +10,30 @@ int main() {
 
   cin.get(str1, 80);
   cin.get();
+  strcpy(str2, str1);
 
-  cout << strlen(str1) << endl; 
+  cout << str1 << endl;
+  cout << str2 << endl;
 
+  cout << str1[0] << endl;
+  cout << str2[strlen(str1) - 1] << endl;
+  if (str1[0] != str2[strlen(str1) - 1]) {
+    cout << "same" << endl;
+  }
+  
+  bool isPalindrome = true;
+  for (int i = 0; i < strlen(str1); i++) {
+    
+    if (str1[i] != str2[strlen(str1) - i - 1]) {
+      isPalindrome = false;
+    }
+  }
+
+  if (isPalindrome == true) {
+    cout << "It's a palindrome!" << endl;
+  } else {
+    cout << "Not a palindrome." << endl;
+  }
 
   return 0;
 }
